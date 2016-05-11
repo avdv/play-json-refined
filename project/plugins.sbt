2a804ab3047e00b5c4c251dfadd477a5c4df8119
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-05-11 21:28:23 +0200

    Reformat source code

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,5 @@
 // Automate file headers
 addSbtPlugin("de.heikoseeberger" % "sbt-header" % "1.5.1")
+
+// Formatting
+addSbtPlugin("com.geirsson" %% "sbt-scalafmt" % "0.2.3")
--- src/main/scala/com.lunaryorn.refined.play.json/package.scala
+++ src/main/scala/com.lunaryorn.refined.play.json/package.scala
@@ -23,20 +23,23 @@ import scala.language.higherKinds
 
 package object play {
   implicit def writeRefined[T, P, F[_, _]](
-    implicit
-    writesT: Writes[T],
-    reftype: RefType[F]
+      implicit writesT: Writes[T],
+      reftype: RefType[F]
   ): Writes[F[T, P]] = Writes(value => writesT.writes(reftype.unwrap(value)))
 
   implicit def readRefined[T, P, F[_, _]](
-    implicit
-    readsT: Reads[T],
-    reftype: RefType[F],
-    validate: Validate[T, P]
-  ): Reads[F[T, P]] = Reads(jsValue => readsT.reads(jsValue).flatMap { valueT =>
-    reftype.refine[P](valueT) match {
-      case Right(valueP) => JsSuccess(valueP)
-      case Left(error) => JsError(error)
-    }
-  })
+      implicit readsT: Reads[T],
+      reftype: RefType[F],
+      validate: Validate[T, P]
+  ): Reads[F[T, P]] =
+    Reads(
+        jsValue =>
+          readsT
+            .reads(jsValue)
+            .flatMap { valueT =>
+          reftype.refine[P](valueT) match {
+            case Right(valueP) => JsSuccess(valueP)
+            case Left(error) => JsError(error)
+          }
+      })
 }
